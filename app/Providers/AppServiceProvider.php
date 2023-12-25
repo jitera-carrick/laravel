94f97d5333789fd4c23e513893cf8fe340fd3c5b
Sophie (Jitera) <developers@jitera.com>
2023-12-25 10:00:41 +0000

    feat[business-logic]: implement 'Reset password request' Changes * '/routes/api.php': + Define a new route that maps to the `sendResetLinkEmail` method in the `ForgotPasswordController`..+ The route should be a POST request to an endpoint such as '/password/email'..+ Ensure the route is within the 'api' middleware group if you want to inclu... * '/app/Providers/AppServiceProvider.php': + In the `boot` method of the `AppServiceProvider`, you can set up any bindings or configurations needed for the password reset functionality..+ For example, if you need to bind an interface to a concrete class for dependency injection in the password rese... * '/config/mail.php': + Make sure the mail configuration is set up correctly to send emails..+ The 'from' address should be set to an appropriate value that emails will be sent from..+ If additional mail configurations are needed for the password reset email, such as a specific...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\PasswordResetInterface;
+use App\Services\PasswordResetService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Bind the PasswordResetInterface to PasswordResetService
+        $this->app->bind(PasswordResetInterface::class, PasswordResetService::class);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgotPasswordController;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Add new route for sending reset password link
+Route::post('/password/email', function (Request $request) {
+    // Validate the input
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['reset_requested' => false, 'errors' => $validator->errors()], 400);
+    }
+
+    // Query the "users" table to find a user with the matching email address
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user) {
+        return response()->json(['reset_requested' => false, 'message' => 'User not found'], 404);
+    }
+
+    // Generate a password reset token and store it
+    $token = Str::random(60);
+    DB::table('password_resets')->insert([
+        'email' => $user->email,
+        'token' => $token,
+        'created_at' => Carbon::now(),
+    ]);
+
+    // Send a password reset email to the user
+    Mail::send('emails.password_reset', ['token' => $token], function ($message) use ($user) {
+        $message->to($user->email);
+        $message->subject('Password Reset Request');
+    });
+
+    // Return a success response
+    return response()->json(['reset_requested' => true]);
+})->middleware('api');
