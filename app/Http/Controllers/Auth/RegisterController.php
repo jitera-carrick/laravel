dd59ad36a04d475218d4dbe542b13150266b26b3
Faydra (Jitera) <developers@jitera.com>
2023-12-25 10:16:30 +0000

    feat[business-logic]: implement 'Send registration confirmation email' Changes * '/app/Http/Controllers/Auth/RegisterController.php': + Implement a new method `sendRegistrationConfirmationEmail` in the `RegisterController` class to handle the sending of the registration confirmation email..+ The method should accept two parameters: `userId` and `token`..+ Use the `User::find` method to r...

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Models\PasswordResetRequest;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 use App\Http\Resources\UserResource;
 use App\Mail\VerifyEmail;
-use Illuminate\Support\Facades\Mail;
+use App\Mail\PasswordResetMail;
+use App\Mail\RegistrationConfirmationMail; // Assuming this Mailable class exists
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
@@ -29,7 +32,7 @@ class RegisterController extends Controller
 
         // Create a new User instance and fill it with the validated data
         $user = new User();
-        $user->name = $validatedData['name'];
+        $user->name = $validatedData['display_name'] ?? $validatedData['name']; // Support both 'name' and 'display_name'
         $user->email = $validatedData['email'];
         $user->password = Hash::make($validatedData['password']); // Encrypt the password
         $user->is_stylist = false; // Set the 'is_stylist' attribute to false
@@ -41,8 +44,39 @@ class RegisterController extends Controller
         // Send a verification email
         Mail::to($user->email)->send(new VerifyEmail($user->remember_token));
 
+        // Generate a unique token for password reset
+        $token = Str::random(60);
+        $passwordResetRequest = new PasswordResetRequest([
+            'user_id' => $user->id,
+            'token' => $token,
+            'expires_at' => now()->addDay(),
+            'status' => 'pending'
+        ]);
+        $passwordResetRequest->save();
+
+        // Send an email to the user with the password reset link
+        Mail::to($user->email)->send(new PasswordResetMail($token));
+
         // Return a UserResource instance as the response
         return (new UserResource($user))->additional(['message' => 'Registration successful, verification email sent.']);
+    }
+
+    public function sendRegistrationConfirmationEmail($userId, $token)
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $emailContent = new RegistrationConfirmationMail($token); // Assuming this Mailable class exists
+
+            Mail::to($user->email)->send($emailContent);
+
+            Log::info('Registration confirmation email sent to user: ' . $user->email);
+
+            return response()->json(['email_sent_status' => 'success'], 200);
+        } catch (\Exception $e) {
+            Log::error('Failed to send registration confirmation email: ' . $e->getMessage());
+
+            return response()->json(['email_sent_status' => 'failed'], 500);
+        }
     }
 
     // Add any other existing methods here
