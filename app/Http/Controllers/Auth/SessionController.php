ac0ea1a8ac1ef397f425fdb69b0d8af24246c8ee
Sukey (Jitera) <developers@jitera.com>
2023-12-25 08:25:08 +0000

    feat[business-logic]: implement 'Maintain user session' Changes * '/app/Http/Controllers/Auth/SessionController.php': + Create a new method `maintainUserSession` in the `SessionController` to handle the request for maintaining a user session..+ This method should accept a `SessionRequest` that includes the `session_token` as input..+ Use the `SessionRequest` to validate t...

---
+++ app/Http/Controllers/Auth/SessionController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SessionRequest;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource as UserResource;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
+class SessionController extends Controller
+{
+    // ... (other methods)
+
+    public function maintainUserSession(SessionRequest $request)
+    {
+        $validated = $request->validated();
+
+        // Check if session_token is provided
+        if (empty($validated['session_token'])) {
+            return response()->json(['error' => 'Session token is required.'], 422);
+        }
+
+        try {
+            $user = User::where('session_token', $validated['session_token'])->first();
+
+            if (!$user) {
+                return response()->json(['error' => 'User not found.'], 404);
+            }
+
+            if (Carbon::now()->lt($user->session_expires)) {
+                $newSessionExpires = $validated['keep_session'] ? Carbon::now()->addDays(90) : Carbon::now()->addDay();
+                $user->session_expires = $newSessionExpires;
+                $user->save();
+
+                return new UserResource($user->only('session_expires'));
+            } else {
+                return response()->json(['error' => 'Session expired.'], 401);
+            }
+        } catch (Exception $e) {
+            // Here you should handle the exception as per your application's exception handling policy
+            // For example, you could use a custom exception class and throw that
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
