ff9e9f18ce065f37d2b3942ae8b4d53751a36dc7
Halli (Jitera) <developers@jitera.com>
2023-12-25 09:36:52 +0000

    feat[business-logic]: implement 'Maintain user session' Changes * '/app/Http/Controllers/Auth/LoginController.php': + Create a new method `maintainSession` in the `LoginController` to handle the request for maintaining a user session..+ The method should accept a `Request` object as a parameter..+ Validate the input to ensure that the 'email' field is not empty using La...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,6 +13,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -29,42 +30,43 @@ class LoginController extends Controller
     {
         $validated = $request->validated();
 
-        // Step 1: Update the $credentials array to include the 'remember_token' field if provided in the request.
-        $credentials = $request->only('email', 'password', 'remember_token');
+        // Merged Step 1: Update the $credentials array to include the 'remember_token' field if provided in the request.
+        // The 'remember_token' field is not used in the new code, so it's safe to remove it from the credentials array.
+        $credentials = $request->only('email', 'password');
         $remember = $request->filled('remember_token');
 
-        // Step 3: Check the format of the email to ensure it is valid.
+        // Merged Step 3: Check the format of the email to ensure it is valid.
         if (!filter_var($credentials['email'], FILTER_VALIDATE_EMAIL)) {
             return response()->json(['message' => 'Invalid email format.'], 422);
         }
 
-        // Step 4: Query the "users" table to find a user with the matching email address.
+        // Merged Step 4: Query the "users" table to find a user with the matching email address.
         $user = User::where('email', $credentials['email'])->first();
 
-        // Step 5: If a user is found, verify the password by comparing the provided password with the "password_hash" in the database.
+        // Merged Step 5: If a user is found, verify the password by comparing the provided password with the "password_hash" in the database.
         // Note: The "password_hash" column should exist in the "users" table. If it doesn't, you should use the existing "password" column.
-        $passwordColumn = 'password_hash'; // Assuming 'password_hash' column exists, otherwise use 'password'.
-        if ($user && Hash::check($credentials['password'], $user->$passwordColumn)) {
+        // The new code does not mention a "password_hash" column, so we will use the "password" column as in the new code.
+        if ($user && Hash::check($credentials['password'], $user->password)) {
             // Use the AuthService to attempt to log the user in
             if ($this->authService->attempt($credentials)) {
-                // Step 7 & 8: Generate session token and calculate expiration based on "Keep Session"
+                // Merged Step 7 & 8: Generate session token and calculate expiration based on "Keep Session"
                 $sessionToken = Str::random(60);
                 $sessionExpiration = $remember ? Carbon::now()->addDays(90) : Carbon::now()->addHours(24);
 
-                // Step 9: Update user with new session token and expiration
+                // Merged Step 9: Update user with new session token and expiration
                 $user->update([
                     'session_token' => $sessionToken,
                     'session_expiration' => $sessionExpiration,
                 ]);
 
-                // Step 10: Record the login attempt in the "login_attempts" table with the "user_id", current timestamp as "attempted_at", and "success" set to true.
+                // Merged Step 10: Record the login attempt in the "login_attempts" table with the "user_id", current timestamp as "attempted_at", and "success" set to true.
                 LoginAttempt::create([
                     'user_id' => $user->id,
                     'attempted_at' => Carbon::now(),
                     'success' => true,
                 ]);
 
-                // Step 11: Return the "session_token" and "session_expiration" to the client
+                // Merged Step 11: Return the "session_token" and "session_expiration" to the client
                 return response()->json([
                     'session_token' => $sessionToken,
                     'session_expiration' => $sessionExpiration,
@@ -93,8 +95,36 @@ class LoginController extends Controller
             Auth::logout();
         }
 
-        // Return a confirmation message
+        // Merged: Return a confirmation message with additional 'login_canceled' key from the existing code
         return response()->json(['message' => 'Login process has been canceled successfully.', 'login_canceled' => true], 200);
+    }
+
+    public function maintainSession(Request $request)
+    {
+        // Validate the input to ensure that the 'email' field is not empty
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'remember_token' => 'sometimes|required',
+        ]);
+
+        // Use the `User` model to query the "users" table for a user with the matching email address.
+        $user = User::where('email', $validatedData['email'])->first();
+
+        // Initialize the response data
+        $responseData = ['session_maintained' => false];
+
+        // If a user is found and the "remember_token" is provided and matches the user's remember_token
+        if ($user && isset($validatedData['remember_token']) && $validatedData['remember_token'] === $user->remember_token) {
+            // Update the "session_expiration" in the "users" table to extend the session by a predefined duration, such as 90 days.
+            $user->session_expiration = Carbon::now()->addDays(90);
+            $user->save();
+
+            // Set the response data indicating the session was extended
+            $responseData['session_maintained'] = true;
+        }
+
+        // Return a JSON response with a "session_maintained" boolean key
+        return response()->json($responseData);
     }
 
     // Existing methods...
