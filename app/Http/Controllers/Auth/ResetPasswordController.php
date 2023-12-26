4c58679f59d2d3dd6dd1bf32eebfe5c4cf95b1df
Tammy (Jitera) <developers@jitera.com>
2023-12-26 05:52:11 +0000

    feat[api]: implement 'Set New Password' Changes * '/app/Http/Controllers/Auth/ResetPasswordController.php': + In the existing ResetPasswordController, add a new method called `setNewPassword` to handle the PUT request for setting a new password..+ Import the `ResetPasswordRequest` class from `/app/Http/Requests/ResetPasswordRequest.php` to use for request valida... * '/routes/api.php': + Add a new route to handle the PUT request for setting a new password..+ The route should be defined as `Route::put('/users/password_reset/set_new_password', [ResetPasswordController::class, 'setNewPassword']);`.+ Ensure that the route is placed within th...

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -15,130 +15,54 @@ use Illuminate\Support\Facades\DB;
 
 class ResetPasswordController extends Controller
 {
+    // ... (other methods remain unchanged)
+
     /**
-     * Validate the password reset token.
+     * Set a new password for the user.
      *
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function validateResetToken(Request $request)
+    public function setNewPassword(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'token' => 'required|string|exists:password_reset_requests,token',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['valid' => false, 'message' => $validator->errors()->all()], 422);
-        }
-
-        $token = $request->token;
-        $passwordResetRequest = PasswordResetRequest::where('token', $token)
-                            ->where('expires_at', '>', now())
-                            ->first();
-
-        if (!$passwordResetRequest) {
-            return response()->json([
-                'valid' => false,
-                'message' => 'The password reset token is invalid or has expired.'
-            ], 404);
-        }
-
-        return response()->json([
-            'valid' => true,
-            'message' => 'The password reset token is valid.'
-        ]);
-    }
-
-    public function reset(Request $request)
-    {
-        // Merge the validation rules and custom error messages from both versions
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|exists:users,email',
-            // Use the most restrictive password policy from the new code
-            'password' => 'required|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&]).+$/|not_in:'.$request->email,
             'token' => 'required|string',
+            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&]).+$/',
         ], [
-            'password.min' => 'The password must be at least 8 characters.',
-            'password.regex' => 'The password must include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*#?&).',
-            'password.not_in' => 'The password cannot be the same as your email address.', // Custom error message for not_in rule
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['message' => 'The provided information is invalid. Please review the requirements and try again.'], 422);
-        }
-
-        try {
-            // Retrieve the PasswordResetRequest using the provided token and ensure it has not expired
-            $passwordResetRequest = PasswordResetRequest::where('token', $request->token)
-                ->where('expires_at', '>', now())
-                ->where('status', '!=', 'completed')
-                ->first();
-
-            if (!$passwordResetRequest) {
-                return response()->json(['message' => 'We cannot process your request at this time. Please request a new password reset link.'], 404);
-            }
-
-            // Find the associated user and update their password
-            $user = User::where('email', $request->email)->first();
-            if (Hash::check($request->password, $user->password)) {
-                return response()->json(['message' => 'The password cannot be the same as the current password.'], 400);
-            }
-
-            $user->password = Hash::make($request->password);
-            $user->save();
-
-            // Invalidate the token
-            $passwordResetRequest->delete();
-
-            // Send a confirmation email to the user
-            Mail::to($user->email)->send(new PasswordResetMail());
-
-            // Always display a message indicating that a password reset email has been sent
-            return response()->json(['message' => 'Your password has been reset successfully. A confirmation email has been sent.'], 200);
-        } catch (Exception $e) {
-            return response()->json(['message' => 'An unexpected error occurred. Please try again later.'], 500);
-        }
-    }
-
-    public function verifyEmailAndSetPassword(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'token' => 'required|string',
-            'password' => 'required|min:8',
+            'token.required' => 'Token is required.',
+            'password.required' => 'Password is required.',
+            'password.min' => 'Password does not meet the required policy.',
+            'password.regex' => 'Password does not meet the required policy.',
         ]);
 
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->all()], 422);
         }
 
-        DB::beginTransaction();
         try {
             $passwordResetRequest = PasswordResetRequest::where('token', $request->token)
                 ->where('expires_at', '>', now())
                 ->first();
 
             if (!$passwordResetRequest) {
-                return response()->json(['password_set_status' => 'invalid_token'], 404);
+                return response()->json(['message' => 'The password reset token is invalid or has expired.'], 400);
             }
 
             $user = User::find($passwordResetRequest->user_id);
             if (!$user) {
-                return response()->json(['password_set_status' => 'user_not_found'], 404);
+                return response()->json(['message' => 'User not found.'], 404);
             }
 
             $user->password = Hash::make($request->password);
-            $user->email_verified_at = now();
             $user->save();
 
             $passwordResetRequest->delete();
 
-            Mail::to($user->email)->send(new \App\Mail\PasswordSetSuccessMail($user)); // Assuming PasswordSetSuccessMail exists
-
-            DB::commit();
-            return response()->json(['password_set_status' => 'success'], 200);
+            return response()->json(['status' => 200, 'message' => 'Your password has been successfully updated.'], 200);
         } catch (Exception $e) {
-            DB::rollBack();
-            return response()->json(['password_set_status' => 'error', 'message' => $e->getMessage()], 500);
+            return response()->json(['message' => 'An unexpected error occurred. Please try again later.'], 500);
         }
     }
+
+    // ... (other methods remain unchanged)
 }
