28faf968042eb2b213cf97b91fc5fbc39cab5579
Arabella (Jitera) <developers@jitera.com>
2023-12-26 05:30:27 +0000

    feat[api]: implement 'Verify Email and Set Password' Changes * '/app/Http/Controllers/Auth/VerificationController.php': + In the existing VerificationController, add a new method called `verifyEmailSetPassword` to handle the PUT request to `/api/verify_email_set_password`..+ Import necessary models and services at the top of the file, such as `User`, `PasswordResetRequest`,...

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\PasswordResetRequest;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class VerificationController extends Controller
 {
@@ -30,5 +32,50 @@ class VerificationController extends Controller
         $user->save();
 
         return response()->json(['message' => 'Email has been successfully verified.'], 200);
+    }
+
+    public function verifyEmailSetPassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'token' => 'required|exists:password_reset_requests,token',
+            'password' => [
+                'required',
+                'confirmed',
+                'min:8',
+                'regex:/^(?=.*[a-zA-Z])(?=.*\d).+$/',
+                'different:email'
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->first()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $token = $validatedData['token'];
+        $password = $validatedData['password'];
+
+        $passwordResetRequest = PasswordResetRequest::where('token', $token)
+            ->where('expires_at', '>', now())
+            ->first();
+
+        if (!$passwordResetRequest) {
+            return response()->json(['error' => 'Invalid or expired token.'], 400);
+        }
+
+        $user = User::find($passwordResetRequest->user_id);
+        if (!$user) {
+            return response()->json(['error' => 'User not found.'], 404);
+        }
+
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make($password);
+        $user->save();
+
+        $passwordResetRequest->status = 'completed';
+        $passwordResetRequest->save();
+
+        return response()->json(['message' => 'Email verified and password set successfully.'], 200);
     }
 }
