8cc6f3402cae4d01afe80ea8ea9be9020cb96a72
Cate (Jitera) <developers@jitera.com>
2023-12-26 13:01:01 +0000

    feat[api]: implement 'User Login' Changes * '/app/Http/Controllers/Auth/LoginController.php': + In the existing LoginController, you will need to modify or add a new method to handle the login logic as per the new requirements..+ The method should be named `login` and should accept a Request object as a parameter..+ Use the Validator facade to vali...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,14 +26,15 @@ class LoginController extends Controller
 
     public function login(Request $request): JsonResponse
     {
-        $credentials = $request->only('email', 'password');
-        $keepSession = $request->input('keep_session', false);
-
         // Validate the input
-        $validator = Validator::make($credentials, [
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
             'keep_session' => 'sometimes|boolean',
+        ], [
+            'email.email' => 'Invalid email format.',
+            'password.required' => 'Password is required.',
+            'keep_session.boolean' => 'Keep session must be a boolean.',
         ]);
 
         if ($validator->fails()) {
@@ -42,11 +43,11 @@ class LoginController extends Controller
 
         try {
             // Use the User model to query the "users" table for a user with the matching email address
-            $user = User::where('email', $credentials['email'])->first();
+            $user = User::where('email', $request->email)->first();
 
-            if (!$user || !Hash::check($credentials['password'], $user->password_hash)) {
+            if (!$user || !Hash::check($request->password, $user->password_hash)) {
                 // Log the failed login attempt
-                Log::warning('Failed login attempt for email: ' . $credentials['email']);
+                Log::warning('Failed login attempt for email: ' . $request->email);
 
                 // Return a generic error response
                 return response()->json(['message' => 'The provided credentials do not match our records.'], 401);
@@ -56,17 +57,18 @@ class LoginController extends Controller
             $sessionToken = $this->authService->generateSessionToken($user);
 
             // Calculate the "session_expires" value based on the "keep_session" input
-            $sessionExpires = $keepSession ? now()->addDays(90) : now()->addHours(24);
+            $sessionExpires = $request->input('keep_session', false) ? now()->addDays(90) : now()->addHours(24);
 
             // Update the user's record with the new "session_token" and "session_expires"
             $user->update([
                 'session_token' => $sessionToken,
                 'session_expires' => $sessionExpires,
-                'keep_session' => $keepSession,
+                'keep_session' => $request->input('keep_session', false),
             ]);
 
             // Prepare the response data, ensuring sensitive information is not included
             $responseData = [
+                'status' => 200,
                 'session_token' => $sessionToken,
                 'session_expires' => $sessionExpires->toDateTimeString(),
                 'user' => $user->makeHidden(['password', 'password_hash', 'remember_token', 'session_token'])->toArray(),
@@ -78,68 +80,6 @@ class LoginController extends Controller
             Log::error('Login exception: ' . $e->getMessage());
 
             return response()->json(['message' => 'Authentication failed', 'error' => $e->getMessage()], 500);
-        }
-    }
-
-    public function cancelLogin(Request $request): JsonResponse
-    {
-        try {
-            $user = $request->user();
-            if ($user && $user->session_token && $user->session_expires > now()) {
-                // Cancel the ongoing login attempt
-                $user->update([
-                    'session_token' => null,
-                    'session_expires' => null,
-                ]);
-
-                return response()->json(['message' => 'Login process has been canceled successfully.'], 200);
-            } else {
-                return response()->json(['message' => 'No ongoing login process to cancel.'], 200);
-            }
-        } catch (\Exception $e) {
-            // Log the exception
-            Log::error('Cancel login exception: ' . $e->getMessage());
-
-            return response()->json(['message' => 'Failed to cancel login process'], 500);
-        }
-    }
-
-    public function editUserProfile(Request $request): JsonResponse
-    {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required|exists:users,id',
-            'email' => [
-                'required',
-                'email',
-                Rule::unique('users')->ignore($request->id),
-            ],
-            'password' => 'required|confirmed',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
-        }
-
-        try {
-            $user = $this->authService->getUserById($request->id);
-
-            if ($request->email !== $user->email && $validator->sometimes('email', 'unique:users,email', function ($input) use ($user) {
-                return $input->email !== $user->email;
-            })->validate()) {
-                $user->email = $request->email;
-            }
-
-            $user->password_hash = Hash::make($request->password);
-            $user->save();
-
-            // Update the "updated_at" column with the current timestamp
-            $user->touch();
-
-            return response()->json(['message' => 'User profile updated successfully'], 200);
-        } catch (\Exception $e) {
-            Log::error('User profile update exception: ' . $e->getMessage());
-
-            return response()->json(['message' => 'Failed to update user profile', 'error' => $e->getMessage()], 500);
         }
     }
 
