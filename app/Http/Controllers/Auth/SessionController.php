6c29e46b9cadd7f2797cf5e366f851dfdb7feb47
Aurlie (Jitera) <developers@jitera.com>
2023-12-26 13:06:31 +0000

    feat[api]: implement 'Validate User Session' Changes * '/app/Http/Controllers/Auth/SessionController.php': + In the existing SessionController, add a new method called `validateUserSession` to handle the GET request for session validation..+ Import the necessary classes at the top of the file if they are not already imported. For example, you might need `use Il...

--- app/Http/Controllers/Auth/SessionController.php
+++ app/Http/Controllers/Auth/SessionController.php
@@ -6,11 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\SessionRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource as UserResource;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Exception;
 
@@ -20,7 +17,6 @@ class SessionController extends Controller
 
     public function maintainUserSession(SessionRequest $request)
     {
-        // Using SessionRequest's validated method to ensure the data is validated
         $validated = $request->validated();
 
         // Check if the user is authenticated and get the authenticated user
@@ -53,33 +49,47 @@ class SessionController extends Controller
     public function logout(Request $request)
     {
         try {
-            // Validate the request
             $validated = $request->validate(['session_token' => 'required|string']);
 
-            // Find the user with the given session token
             $user = User::where('session_token', $validated['session_token'])->first();
 
-            // If the user is not found, return an error
             if (!$user) {
                 return response()->json(['error' => 'User not found.'], 404);
             }
 
-            // Clear the session token and expiration
             $user->session_token = null;
             $user->session_expires = Carbon::now();
             $user->save();
 
-            // Return a successful logout response
             return response()->json(['message' => 'Logged out successfully.']);
         } catch (Exception $e) {
-            // If there is an exception, return an error
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
     public function validateUserSession(Request $request)
     {
-        // ... (existing code for validateUserSession)
+        $sessionToken = $request->query('session_token');
+
+        if (!$sessionToken) {
+            return response()->json(['error' => 'Session token is required.'], 400);
+        }
+
+        try {
+            $user = User::where('session_token', $sessionToken)->first();
+
+            if ($user && $user->session_expires && $user->session_expires->isFuture()) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Session is valid.',
+                    'user_id' => $user->id
+                ], 200);
+            } else {
+                return response()->json(['error' => 'Invalid session token.'], 401);
+            }
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Internal Server Error'], 500);
+        }
     }
 
     public function verifyEmail($id, $verification_token)
@@ -89,26 +99,21 @@ class SessionController extends Controller
 
     public function updateUserProfile(Request $request)
     {
-        // Use Validator facade to validate the request
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email,' . $request->user()->id,
         ]);
 
-        // If validation fails, return an error response
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        // Get the authenticated user
         $user = $request->user();
 
-        // Update the user's name and email
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->save();
 
-        // Return a successful profile update response
         return response()->json([
             'status' => 200,
             'message' => 'Profile updated successfully.',
