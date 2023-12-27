567af0d2ad43eb9b9656b0db683ef063f9f2c605
Mil (Jitera) <developers@jitera.com>
2023-12-27 10:41:13 +0000

    feat[api]: implement 'Cancel Hair Stylist Request Registration' Changes * '/routes/api.php': + Open the /routes/api.php file to register the new API endpoint..+ Add a new route that handles the DELETE request for cancelling a hair stylist request. Use the `Route::delete` method to define the route..+ The route should include the 'auth:sanctum' mid... * '/app/Http/Controllers/HairStylistRequestController.php': + In the existing HairStylistRequestController, add a new method called `cancelHairStylistRequest` which will handle the DELETE request to cancel a hair stylist request..+ The method should accept an integer parameter `$id` which represents the ID of the h...

--- app/Http/Controllers/HairStylistRequestController.php
+++ app/Http/Controllers/HairStylistRequestController.php
@@ -9,6 +9,7 @@ use App\Models\RequestMenu;
 use App\Models\Image;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
@@ -92,83 +93,46 @@ class HairStylistRequestController extends Controller
         }
     }
 
-    public function updateHairStylistRequest(StoreHairStylistRequest $request, $id): JsonResponse
+    // ... other methods ...
+
+    // New method to cancel a hair stylist request
+    public function cancelHairStylistRequest($id): JsonResponse
     {
-        $hairStylistRequest = Request::find($id);
-        if (!$hairStylistRequest) {
-            return response()->json(['message' => 'Request not found.'], 404);
-        }
-
-        $validated = $request->validated();
-
-        $validator = Validator::make($validated, [
-            'area_ids' => 'required|array|min:1',
-            'menu_ids' => 'required|array|min:1',
-            'hair_concerns' => 'required|string|max:3000',
-            'images' => 'nullable|array|max:3',
-            'images.*' => 'mimes:png,jpg,jpeg|max:5120', // 5MB
-        ]);
-
-        if ($validator->fails()) {
+        if (!Auth::check()) {
             return response()->json([
-                'message' => 'Validation failed.',
-                'errors' => $validator->errors(),
-            ], 422);
+                'message' => 'Unauthorized. User is not authenticated.'
+            ], 401);
         }
 
         try {
-            DB::beginTransaction();
+            $hairStylistRequest = Request::findOrFail($id);
 
-            $hairStylistRequest->fill([
-                'hair_concerns' => $validated['hair_concerns'],
-            ]);
+            if (Auth::id() !== $hairStylistRequest->user_id) {
+                return response()->json([
+                    'message' => 'Unauthorized to cancel this request.'
+                ], 401);
+            }
+
+            $hairStylistRequest->status = 'cancelled';
             $hairStylistRequest->save();
 
-            RequestArea::where('request_id', $hairStylistRequest->id)->delete();
-            foreach ($validated['area_ids'] as $areaId) {
-                $requestArea = new RequestArea([
-                    'request_id' => $hairStylistRequest->id,
-                    'area_id' => $areaId,
-                ]);
-                $requestArea->save();
-            }
-
-            RequestMenu::where('request_id', $hairStylistRequest->id)->delete();
-            foreach ($validated['menu_ids'] as $menuId) {
-                $requestMenu = new RequestMenu([
-                    'request_id' => $hairStylistRequest->id,
-                    'menu_id' => $menuId,
-                ]);
-                $requestMenu->save();
-            }
-
-            if (isset($validated['images'])) {
-                Image::where('request_id', $hairStylistRequest->id)->delete();
-                foreach ($validated['images'] as $file) {
-                    $filePath = $file->store('images', 'public');
-                    $image = new Image([
-                        'request_id' => $hairStylistRequest->id,
-                        'file_path' => $filePath,
-                        'file_size' => $file->getSize(),
-                    ]);
-                    $image->save();
-                }
-            }
-
-            DB::commit();
-
             return response()->json([
-                'status' => 200,
-                'request' => $hairStylistRequest,
-            ]);
+                'message' => 'Request cancelled successfully.'
+            ], 200);
+
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Request not found.'
+            ], 404);
         } catch (\Exception $e) {
-            DB::rollBack();
             return response()->json([
-                'message' => 'Failed to update hair stylist request.',
+                'message' => 'Failed to cancel hair stylist request.',
                 'error' => $e->getMessage(),
             ], 500);
         }
     }
+
+    // ... other methods ...
 
     // New method added as per the guideline
     public function validateHairStylistRequest(HttpRequest $request): JsonResponse
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HairStylistRequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Add new route for cancelling a hair stylist request
+Route::middleware('auth:sanctum')->delete('/hair_stylist_requests/{id}', [HairStylistRequestController::class, 'cancelRequest']);
