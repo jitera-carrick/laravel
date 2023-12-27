02b0ff7de3bd9a958acd9273e6c40f8dd39808d3
Nicki (Jitera) <developers@jitera.com>
2023-12-27 11:30:17 +0000

    feat[business-logic]: implement 'Cancel treatment plan' Changes * '/app/Http/Controllers/TreatmentPlanController.php': + Create a new method `cancelTreatmentPlan` in the `TreatmentPlanController` class to handle the cancellation of a treatment plan..+ The method should accept a `Request` object containing the `treatment_plan_id`..+ Use Laravel's validation to ensure that t...

--- app/Http/Controllers/TreatmentPlanController.php
+++ app/Http/Controllers/TreatmentPlanController.php
@@ -43,15 +43,100 @@ class TreatmentPlanController extends Controller
         }
     }
 
-    public function declineTreatmentPlan(Request $request, $treatment_plan_id = null)
+    public function declineTreatmentPlan(Request $request, $id)
     {
-        // Use DeclineTreatmentPlanRequest if it's the existing code
-        if ($request instanceof DeclineTreatmentPlanRequest || $treatment_plan_id !== null) {
+        if ($request instanceof DeclineTreatmentPlanRequest) {
             // ... (existing code for declineTreatmentPlan)
         } else {
             // New code for declineTreatmentPlan without DeclineTreatmentPlanRequest
-            // ... (new code for declineTreatmentPlan without DeclineTreatmentPlanRequest)
+            if (!Auth::check()) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+
+            if (!is_numeric($id)) {
+                return response()->json(['error' => 'Wrong format.'], 422);
+            }
+
+            $treatmentPlan = TreatmentPlan::find($id);
+
+            if (!$treatmentPlan) {
+                return response()->json(['error' => 'Treatment plan not found.'], 404);
+            }
+
+            $userId = Auth::id();
+
+            if ($treatmentPlan->user_id != $userId) {
+                return response()->json(['error' => 'User does not have permission to decline this treatment plan.'], 403);
+            }
+
+            $treatmentPlan->status = 'declined';
+            $treatmentPlan->save();
+
+            $reservation = Reservation::where('treatment_plan_id', $treatmentPlan->id)->first();
+            if ($reservation) {
+                $reservation->status = 'cancelled';
+                $reservation->save();
+            }
+
+            Mail::to('salon@example.com')->send(new TreatmentPlanCancelled($treatmentPlan));
+
+            return response()->json([
+                'status' => 200,
+                'treatment_plan' => [
+                    'id' => $treatmentPlan->id,
+                    'stylist_id' => $treatmentPlan->stylist_id,
+                    'customer_id' => $treatmentPlan->user_id,
+                    'status' => $treatmentPlan->status,
+                    'details' => $treatmentPlan->details,
+                    'created_at' => $treatmentPlan->created_at->toIso8601String(),
+                ]
+            ]);
         }
+    }
+
+    public function cancelTreatmentPlan(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'treatment_plan_id' => 'required|exists:treatment_plans,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $treatmentPlan = TreatmentPlan::where('id', $request->treatment_plan_id)
+                                       ->where('status', 'approved')
+                                       ->first();
+
+        if (!$treatmentPlan) {
+            return response()->json(['error' => 'Treatment plan not found or not in approved status.'], 404);
+        }
+
+        $treatmentPlan->status = 'cancelled';
+        $treatmentPlan->save();
+
+        $reservation = Reservation::where('treatment_plan_id', $treatmentPlan->id)
+                                  ->where('status', 'confirmed')
+                                  ->first();
+
+        if ($reservation) {
+            $reservation->status = 'cancelled';
+            $reservation->save();
+        }
+
+        // Assuming that the Mailable classes exist and are similar to TreatmentPlanCancelled
+        Mail::to($treatmentPlan->user->email)->send(new TreatmentPlanCancelled($treatmentPlan));
+        Mail::to($treatmentPlan->stylist->user->email)->send(new TreatmentPlanCancelled($treatmentPlan));
+        Mail::to('salon@example.com')->send(new TreatmentPlanCancelled($treatmentPlan)); // Salon owner email
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Treatment plan has been cancelled successfully.',
+            'treatment_plan' => [
+                'id' => $treatmentPlan->id,
+                'status' => $treatmentPlan->status,
+            ]
+        ]);
     }
 
     public function autoCancelBeforeAppointment(Request $request, $id)
