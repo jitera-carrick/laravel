f95cd70080694572b64cbf42fc37318672bf739d
Ketty (Jitera) <developers@jitera.com>
2023-12-27 07:54:19 +0000

    feat[business-logic]: implement 'Maintain user session' Changes * '/app/Http/Controllers/Auth/LoginController.php': + In the `LoginController`, add a new method `maintainUserSession` with the following signature: `public function maintainUserSession(Request $request)`..+ Validate the request to ensure `user_id` is present. Use the `Validator` facade as shown in the `Res...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,183 +11,63 @@ use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Session;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
-    // Combined attemptLogin method with logging from existing code
-    protected function attemptLogin(array $credentials, $keepSession)
-    {
-        if (Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
-            $this->handleUserSession(Auth::user(), $keepSession);
-            return true;
-        }
-
-        // Log the failed login attempt from existing code
-        Log::warning('Login attempt failed for email: ' . $credentials['email']);
-
-        return false;
-    }
-
-    // Combined handleUserSession method with logic from both new and existing code
-    protected function handleUserSession(User $user, $keepSession)
-    {
-        // Determine the session_expiration time based on whether the keep_session flag is set
-        $expirationTime = $keepSession ? Carbon::now()->addDays(90) : Carbon::now()->addHours(24);
-        $sessionToken = bin2hex(openssl_random_pseudo_bytes(30)); // Use the new code's method for generating a session token
-
-        // Create or update the session in the "sessions" table
-        $session = Session::updateOrCreate(
-            ['user_id' => $user->id],
-            [
-                'session_token' => $sessionToken,
-                'expires_at' => $expirationTime // Use expires_at from new code
-            ]
-        );
-
-        // Update the user's session_token attribute
-        $user->session_token = $sessionToken;
-        $user->save();
-    }
+    // ... Other methods in the LoginController
 
     /**
-     * Handle the API login request.
-     *
-     * @param \App\Http\Requests\LoginRequest $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function apiLogin(LoginRequest $request)
-    {
-        $credentials = $request->only('email', 'password');
-        $remember = $request->input('remember', false);
-
-        if ($this->attemptLogin($credentials, $remember)) {
-            $user = Auth::user();
-            $token = $user->createToken('authToken')->accessToken;
-            $expiration = $remember ? Carbon::now()->addDays(90) : Carbon::now()->addHours(24);
-
-            return response()->json([
-                'status' => 200,
-                'message' => 'Login successful.',
-                'session_token' => $token,
-                'session_expiration' => $expiration->toIso8601String()
-            ]);
-        } else {
-            return $this->handleLoginFailure($request);
-        }
-    }
-
-    /**
-     * Handle the login request.
-     *
-     * @param LoginRequest $request
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
-    public function login(LoginRequest $request)
-    {
-        $credentials = $request->only('email', 'password');
-        $keepSession = $request->input('keep_session', false);
-
-        if ($this->attemptLogin($credentials, $keepSession)) {
-            $user = Auth::user();
-
-            if ($request->wantsJson()) {
-                $token = $user->createToken('authToken')->accessToken;
-
-                return response()->json([
-                    'message' => 'Login successful.',
-                    'token' => $token
-                ]);
-            } else {
-                return redirect()->intended('screen-menu_user')->with([
-                    'session_token' => $user->session_token,
-                ]);
-            }
-        } else {
-            return $this->handleLoginFailure($request);
-        }
-    }
-
-    /**
-     * Handle login failures by providing an appropriate error message.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function handleLoginFailure(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        $user = User::where('email', $credentials['email'])->first();
-
-        if (!$user) {
-            return response()->json([
-                'status' => 401,
-                'message' => 'Account not found.'
-            ], 401);
-        }
-
-        if (!Hash::check($credentials['password'], $user->password)) {
-            return response()->json([
-                'status' => 401,
-                'message' => 'Incorrect password.'
-            ], 401);
-        }
-
-        return response()->json([
-            'status' => 500,
-            'message' => 'An unexpected error occurred on the server.'
-        ], 500);
-    }
-
-    /**
-     * Maintain the user session based on the session token and keepSession flag.
+     * Maintain the user session based on the user_id and session_token.
      *
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function maintainUserSession(Request $request)
     {
-        $sessionToken = $request->input('session_token');
-        $keepSession = $request->input('keep_session', false);
-
-        // Validate the "session_token" and "keep_session" parameters
-        $request->validate([
-            'session_token' => 'required|exists:sessions,session_token',
-            'keep_session' => 'required|boolean',
+        // Validate the request to ensure user_id is present
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'session_token' => 'sometimes|exists:sessions,session_token'
         ]);
 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
         try {
-            $session = Session::where('session_token', $sessionToken)->first();
+            // Retrieve the session using the Session model and the provided user_id
+            $query = Session::where('user_id', $request->user_id);
+            if ($request->has('session_token')) {
+                $query->where('session_token', $request->session_token);
+            }
+            $session = $query->first();
 
             if (!$session) {
-                return response()->json(['message' => 'Invalid session token.'], 400);
+                return response()->json(['message' => 'Session not found.'], 404);
             }
 
             if ($session->expires_at->isPast()) {
                 return response()->json(['message' => 'Session has expired.'], 401);
             }
 
-            if ($keepSession) {
-                $session->expires_at = Carbon::now()->addDays(90);
-                $session->save();
+            // Calculate the new expiration date based on the keep_session attribute
+            $newExpirationDate = $session->keep_session ? Carbon::now()->addDays(90) : Carbon::now()->addHours(24);
 
-                return response()->json([
-                    'status' => 200,
-                    'message' => 'Session maintained successfully.',
-                    'session_expiration' => $session->expires_at->toIso8601String()
-                ]);
-            }
+            // Update the expires_at field in the session record
+            $session->expires_at = $newExpirationDate;
+            $session->save();
 
-            return response()->json(['message' => 'Session remains unchanged.']);
+            return response()->json([
+                'status' => 200,
+                'message' => 'User session has been maintained.',
+                'new_expiration' => $newExpirationDate->toIso8601String()
+            ]);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'An error occurred while maintaining the session: ' . $e->getMessage()], 500);
+            Log::error('Error maintaining user session: ' . $e->getMessage());
+            return response()->json(['error' => 'An error occurred while maintaining the session.'], 500);
         }
-    }
-
-    public function cancelLoginProcess()
-    {
-        // ... Existing cancelLoginProcess method code
     }
 
     // ... Rest of the existing code in the LoginController
