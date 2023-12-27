e2b04b247bc4549e293650ebee7c81cf0701ae4e
Myrta (Jitera) <developers@jitera.com>
2023-12-27 04:43:57 +0000

    feat[business-logic]: implement 'Password reset request' Changes * '/app/Http/Controllers/Auth/ForgotPasswordController.php': + Create a new method `sendResetLinkEmail` in the `ForgotPasswordController` that accepts a `Request` object containing the user's email..+ Use Laravel's built-in validation to ensure the email field is present and formatted correctly. You can use `$reques...

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Str;
-use App\Models\PasswordResetToken;
+use App\Models\PasswordReset;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Support\Facades\DB;
@@ -27,27 +27,31 @@ class ForgotPasswordController extends Controller
         // Validate the email field
         $request->validate(['email' => 'required|email']);
 
-        // Check if the email exists in the database
+        // Find the user by email
         $user = User::where('email', $request->email)->first();
 
         if ($user) {
-            // Generate a unique token
+            // Generate a unique reset token and expiration time
             $token = Str::random(60);
+            $expiration = Carbon::now()->addMinutes(60);
 
-            // Store the token in the password_reset_tokens table
-            PasswordResetToken::create([
-                'email' => $user->email,
-                'token' => $token,
-                'created_at' => now(),
-                'user_id' => $user->id
+            // Create a new entry in the password_resets table
+            // Check if the table name is 'password_resets' or 'password_reset_tokens'
+            // and use the appropriate model.
+            $passwordReset = PasswordReset::create([
+                'email' => $user->email, // Ensure 'email' field is included for compatibility
+                'token' => $token, // Use 'token' instead of 'reset_token' for compatibility
+                'created_at' => now(), // Use 'created_at' for compatibility
+                'expiration' => $expiration, // Additional field for new feature
+                'status' => 'pending' // Additional field for new feature
             ]);
 
             // Send the password reset email
             $user->notify(new ResetPasswordNotification($token));
         }
 
-        // Return a response indicating that the email has been sent
-        return response()->json(['message' => 'A password reset email has been sent.'], 200);
+        // Return a response with a generic message
+        return response()->json(['message' => 'If an account with that email exists, we have sent a password reset link to your email address.']);
     }
 
     /**
@@ -58,15 +62,15 @@ class ForgotPasswordController extends Controller
      */
     public function validateResetToken(string $token): bool
     {
-        $tokenRecord = DB::table('password_reset_tokens')->where('token', $token)->first();
+        $tokenRecord = DB::table('password_resets')->where('token', $token)->first(); // Use 'password_resets' table
 
         if (!$tokenRecord) {
             return false;
         }
 
-        $tokenCreationTime = Carbon::parse($tokenRecord->created_at);
-        $expireMinutes = config('auth.passwords.users.expire');
-        $isTokenExpired = $tokenCreationTime->addMinutes($expireMinutes)->isPast();
+        // Check if the 'expiration' field exists and use it to determine if the token is expired
+        $tokenExpirationTime = isset($tokenRecord->expiration) ? Carbon::parse($tokenRecord->expiration) : Carbon::parse($tokenRecord->created_at)->addMinutes(config('auth.passwords.users.expire'));
+        $isTokenExpired = $tokenExpirationTime->isPast();
 
         return !$isTokenExpired;
     }
