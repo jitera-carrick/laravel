09c46572fcd1b799533ebce32c90523ca53ad5d0
Gertruda (Jitera) <developers@jitera.com>
2023-12-27 11:52:04 +0000

    feat[api]: implement 'Send Message and Notify' Changes * '/app/Http/Controllers/MessageController.php': + In the existing MessageController, add a new method called `sendUserMessageAndNotify`..+ Import necessary models at the top of the file if they are not already imported. For example, `use App\Models\User;` and `use App\Models\Message;`..+ Use the `Auth` ...

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,6 +13,8 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use App\Events\MessageSent; // Import the MessageSent event
+use Illuminate\Support\Facades\Event; // Import the Event facade
 
 class MessageController extends Controller
 {
@@ -30,7 +32,53 @@ class MessageController extends Controller
         // ...
     }
 
-    // ... other methods ...
+    // New method as per the requirement
+    public function sendUserMessageAndNotify(Request $request)
+    {
+        // Authentication check
+        if (!Auth::check()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        // Validation
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|string|max:500',
+            'user_id' => 'required|exists:users,id',
+            'recipient_id' => 'required|exists:users,id',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            if ($errors->has('content')) {
+                return response()->json(['error' => 'You cannot input more than 500 characters.'], 400);
+            }
+            if ($errors->has('user_id')) {
+                return response()->json(['error' => 'User not found.'], 400);
+            }
+            if ($errors->has('recipient_id')) {
+                return response()->json(['error' => 'Recipient not found.'], 400);
+            }
+            return response()->json(['error' => $validator->errors()->first()], 400);
+        }
+
+        // Create and save the message
+        $message = new Message([
+            'content' => $request->content,
+            'user_id' => $request->user_id,
+            'receiver_id' => $request->recipient_id,
+            'sent_at' => Carbon::now(),
+        ]);
+        $message->save();
+
+        // Trigger notification event
+        Event::dispatch(new MessageSent($message));
+
+        // Return success response
+        return response()->json([
+            'status' => 200,
+            'message' => 'Message sent successfully and notification triggered.'
+        ]);
+    }
 
     public function logMessageFailure(Request $request)
     {
@@ -76,4 +124,6 @@ class MessageController extends Controller
             'message' => 'Message sending failure has been logged and handled.',
         ]);
     }
+
+    // ... other methods ...
 }
