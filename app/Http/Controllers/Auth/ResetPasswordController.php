4bfd97d1f6cd0fcac035542365385923783b3047
Charmain (Jitera) <developers@jitera.com>
2023-12-27 04:55:29 +0000

    feat[business-logic]: implement 'Validate password reset link' Changes * '/app/Http/Controllers/Auth/ResetPasswordController.php': + Create a new method `validateResetToken` in the `ResetPasswordController` with input parameter "token" which is a string..+ Use the `PasswordResetToken` model to query the `password_reset_tokens` table to check if the token exists and has not expired..+ ...

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -12,6 +12,8 @@ use App\Models\PasswordPolicy;
 use App\Models\PasswordReset;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Carbon;
 
 class ResetPasswordController extends Controller
 {
@@ -35,8 +37,8 @@ class ResetPasswordController extends Controller
 
         // Validation rules
         $rules = [
-            'email' => 'required|email|exists:users,email',
-            'token' => 'required',
+            'email' => 'required|email|exists:users,email', // Use 'exists:users,email' to ensure the user exists
+            'token' => 'required|exists:password_reset_tokens,token', // Keep the token validation rule from the new code
             'password' => [
                 'required',
                 'confirmed',
@@ -79,9 +81,27 @@ class ResetPasswordController extends Controller
         $tokenData->delete();
 
         // Trigger a ResetPasswordNotification
-        Notification::send($user, new ResetPasswordNotification());
+        Notification::send($user, new ResetPasswordNotification()); // Use Notification facade as in the existing code
 
         // Return a success response
         return response()->json(['message' => 'Password has been successfully reset.']);
+    }
+
+    // New method to validate reset token
+    public function validateResetToken($token)
+    {
+        try {
+            $passwordResetToken = PasswordResetToken::where('token', $token)->firstOrFail();
+
+            if ($passwordResetToken->expires_at < Carbon::now()) {
+                return response()->json(['message' => 'Token has expired'], 404);
+            }
+
+            return response()->json(['message' => 'Token is valid'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Token is invalid'], 400);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred while validating the token'], 500);
+        }
     }
 }
