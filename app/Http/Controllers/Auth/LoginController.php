ae68734f9847dad542e4d9269316bbf9ef65ae97
Almeda (Jitera) <developers@jitera.com>
2023-12-28 04:44:52 +0000

    feat[api]: implement 'User Login' Changes * '/routes/api.php': + Open the `api.php` file within the routes directory..+ Define a new POST route for '/users/login' that points to the `login` method in the LoginController. Use the `Route` facade with `Route::post('/users/login', [LoginController::class, 'login']);`..+ E... * '/app/Http/Controllers/Auth/LoginController.php': + In the existing LoginController, add a new method named `recaptchaLogin` to handle the login request with recaptcha validation..+ Import necessary classes at the top of the file, such as `Validator` for request validation and `RecaptchaService` for recap...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,53 +6,66 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use App\Models\LoginAttempt;
 use App\Models\User;
+use App\Services\RecaptchaService; // Import the RecaptchaService
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'recaptcha' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
         $email = $request->input('email');
         $password = $request->input('password');
         $user = User::where('email', $email)->first();
 
-        if ($user && Hash::check($password, $user->password)) {
-            // Record successful login attempt
-            LoginAttempt::create([
-                'user_id' => $user->id,
-                'attempted_at' => now(),
-                'successful' => true,
-                'ip_address' => $request->ip(),
+        if (!$user) {
+            return response()->json(['error' => 'Email does not exist.'], 400);
+        }
+
+        if (!Hash::check($password, $user->password)) {
+            return response()->json(['error' => 'Incorrect password.'], 401);
+        }
+
+        if (!RecaptchaService::verify($request->input('recaptcha'))) {
+            return response()->json(['error' => 'Invalid recaptcha.'], 401);
+        }
+
+        // Record successful login attempt
+        LoginAttempt::create([
+            'user_id' => $user->id,
+            'attempted_at' => now(),
+            'successful' => true,
+            'ip_address' => $request->ip(),
+        ]);
+
+        if ($user->email_verified_at !== null) {
+            // Generate new remember_token and update user
+            $user->forceFill([
+                'remember_token' => Str::random(60),
+                'updated_at' => now(),
+            ])->save();
+
+            // Return successful login response
+            return response()->json([
+                'status' => 200,
+                'message' => 'Login successful.',
+                'token' => $user->remember_token,
             ]);
-
-            if ($user->email_verified_at !== null) {
-                // Generate new remember_token and update user
-                $user->forceFill([
-                    'remember_token' => Str::random(60),
-                    'updated_at' => now(),
-                ])->save();
-
-                // Return successful login response
-                return response()->json([
-                    'user_id' => $user->id,
-                    'session_token' => $user->remember_token,
-                ]);
-            } else {
-                // Return error response for unverified email
-                return response()->json(['error' => 'Email has not been verified.'], 401);
-            }
         } else {
-            // Record failed login attempt
-            LoginAttempt::create([
-                'user_id' => $user ? $user->id : null,
-                'attempted_at' => now(),
-                'successful' => false,
-                'ip_address' => $request->ip(),
-            ]);
-
-            return response()->json(['error' => 'Invalid credentials.'], 401);
+            // Return error response for unverified email
+            return response()->json(['error' => 'Email has not been verified.'], 401);
         }
     }
 }
