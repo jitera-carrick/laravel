e6963bea80c83147e755804317f7908845d12834
Maegan (Jitera) <developers@jitera.com>
2023-12-28 03:31:00 +0000

    feat[api]: implement 'Edit User Profile' Changes * '/app/Http/Controllers/UserController.php': + In the UserController, create a new method called `updateUserProfile` which accepts an instance of a custom request class, say `UpdateUserProfileRequest`. This request class will handle the validation logic for the 'name' and 'email' fields..+ Import the... * '/routes/api.php': + In the `api.php` file, you need to define a new route that handles the PUT request to the endpoint `/api/users/profile`..+ Use the `Route::put` method to define the route and point it to the `updateProfile` method in the `UserController`..+ Ensure that t...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,8 @@ class UserController extends Controller
     public function updateUserProfile(UpdateUserProfileRequest $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'new_password' => 'required|confirmed',
+            'name' => 'required|string',
+            'email' => 'required|email',
         ]);
 
         if ($validator->fails()) {
@@ -28,27 +29,27 @@ class UserController extends Controller
         }
 
         try {
-            $user = $this->userService->findUserById($request->user_id);
+            $user = $this->userService->findUserById($request->user()->id);
 
-            if (!Hash::check($request->current_password, $user->password)) {
-                return response()->json(['message' => 'Current password is incorrect.'], 400);
+            if ($request->email !== $user->email && !$this->userService->isEmailUnique($request->email, $user->id)) {
+                return response()->json(['message' => 'Email already in use by another account.'], 400);
             }
 
-            if ($request->email !== $user->email && !$this->userService->isEmailUnique($request->email)) {
-                return response()->json(['message' => 'Email is already in use.'], 400);
-            }
-
-            $user->password = Hash::make($request->new_password);
+            $user->name = $request->name;
             $user->email = $request->email;
-            $user->email_verified_at = null;
             $user->updated_at = now();
             $user->save();
 
-            if ($request->email !== $user->email) {
-                $this->userService->sendProfileUpdateConfirmation($user);
-            }
-
-            return response()->json(['message' => 'User profile updated successfully.'], 200);
+            return response()->json([
+                'status' => 200,
+                'message' => 'Profile updated successfully.',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'updated_at' => $user->updated_at->toIso8601String(),
+                ]
+            ], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Failed to update user profile.'], 422);
         }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // Import UserController
+use App\Http\Controllers\ShopController; // Import ShopController
 
 /*
 |--------------------------------------------------------------------------
@@ -9,18 +11,17 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned to the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-// Existing route to get user details
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // New route for updating shop details
-Route::middleware('auth:sanctum')->match(['put', 'patch'], '/shop/update', 'ShopController@updateShop');
+Route::middleware('auth:sanctum')->match(['put', 'patch'], '/shop/update', [ShopController::class, 'updateShop']);
 
-// New route for email verification
-Route::get('/users/verify/{token}', 'Auth\VerificationController@verifyEmail')->name('verification.verify');
+// New route for updating user profile
+Route::middleware('auth:sanctum')->put('/users/profile', [UserController::class, 'updateProfile']);
