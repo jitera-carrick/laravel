ce44c86f8fea7a9c2396a56458fc499e92e22362
Sharai (Jitera) <developers@jitera.com>
2023-12-28 04:10:30 +0000

    feat[api]: implement 'Generate Password Reset Token' Changes * '/app/Http/Controllers/Auth/ForgotPasswordController.php': + In the ForgotPasswordController, you need to add a new method called `generateResetToken` which will handle the logic for generating a password reset token and sending it to the user's email..+ Import the necessary models and facades at the top of the fi...

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 use App\Models\PasswordResetToken;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
     // ... (other methods in the controller)
 
-    // Add the new method below
+    // Existing method for verifying reset token
     public function verifyResetToken(Request $request)
     {
         try {
@@ -39,6 +42,56 @@ class ForgotPasswordController extends Controller
             return Response::json([
                 'status' => 'error',
                 'message' => 'An error occurred while verifying the token.'
+            ], 500);
+        }
+    }
+
+    // Updated method for generating reset token
+    public function generateResetToken(Request $request)
+    {
+        try {
+            $request->validate(['email' => 'required|email']);
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user) {
+                return Response::json([
+                    'status' => 'error',
+                    'message' => 'Email address not found.'
+                ], 404);
+            }
+
+            $token = Str::random(60);
+
+            $passwordResetToken = new PasswordResetToken([
+                'email' => $user->email,
+                'token' => $token,
+                'expires_at' => now()->addHours(24),
+                'used' => false,
+                'user_id' => $user->id
+            ]);
+
+            $passwordResetToken->save();
+
+            Mail::send('emails.password_reset', ['token' => $token], function ($message) use ($user) {
+                $message->to($user->email);
+                $message->subject('Your Password Reset Token');
+            });
+
+            return Response::json([
+                'status' => 'success',
+                'message' => 'A password reset token has been sent to your email address.'
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return Response::json([
+                'status' => 'error',
+                'message' => 'Invalid email address.',
+                'errors' => $e->errors(),
+            ], 400);
+        } catch (\Exception $e) {
+            return Response::json([
+                'status' => 'error',
+                'message' => 'An error occurred while generating the password reset token.'
             ], 500);
         }
     }
