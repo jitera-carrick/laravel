4d1aba44b9a422f1c16d1dc585c07e8a9dc9d972
Dania (Jitera) <developers@jitera.com>
2023-12-28 04:34:10 +0000

    feat[api]: implement 'Password Reset Confirmation' Changes * '/routes/api.php': + In the `/routes/api.php` file, you need to define a new POST route for the password reset confirmation functionality..+ Import the `PasswordResetConfirmationController` at the top of the file if it's not already imported..+ Use the `Route::post` method t...

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ForgotPasswordController; // Import the ForgotPasswordController
+use App\Http\Controllers\Auth\PasswordResetConfirmationController; // Import the PasswordResetConfirmationController
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Password;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +47,59 @@ Route::post('/users/password-reset-request', function (Request $request) {
     // Assuming the ForgotPasswordController's sendResetLinkEmail method handles the response
     return app(ForgotPasswordController::class)->sendResetLinkEmail($request);
 })->name('password.email');
+
+// New route for password reset confirmation
+Route::post('/users/password-reset-confirmation', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'token' => 'required',
+        'password' => 'required|min:8',
+    ], [
+        'token.required' => 'Token is required.',
+        'password.required' => 'Password is required.',
+        'password.min' => 'Password must be at least 8 characters long.',
+    ]);
+
+    if ($validator->fails()) {
+        $errors = $validator->errors();
+        $status = 400;
+        $message = 'Invalid request parameters.';
+
+        if ($errors->has('token')) {
+            $status = 404;
+            $message = $errors->first('token');
+        } elseif ($errors->has('password')) {
+            $status = 422;
+            $message = $errors->first('password');
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+        ], $status);
+    }
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => bcrypt($password)
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        }
+    );
+
+    if ($status == Password::PASSWORD_RESET) {
+        return response()->json([
+            'status' => 200,
+            'message' => 'Password reset successfully.',
+        ]);
+    } else {
+        return response()->json([
+            'status' => 400,
+            'message' => 'Invalid or expired password reset token.',
+        ], 400);
+    }
+})->name('password.reset.confirm');
