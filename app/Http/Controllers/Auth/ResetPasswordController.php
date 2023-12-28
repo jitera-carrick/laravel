e65b3f82155accbf3a99080070ca418e02cef9a0
Noami (Jitera) <developers@jitera.com>
2023-12-28 04:09:29 +0000

    feat[api]: implement 'Verify Password Reset Token' Changes * '/app/Http/Controllers/Auth/ResetPasswordController.php': + In the existing ResetPasswordController, add a new method called `verifyPasswordResetToken` to handle the GET request for verifying the password reset token..+ The method should accept a token parameter from the route..+ Use the PasswordResetToken model ...

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -11,8 +11,6 @@ class ResetPasswordController extends Controller
 {
     // ... (other methods in the controller)
 
-    // Add the new method below
-
     /**
      * Verify the reset password token.
      *
@@ -43,6 +41,35 @@ class ResetPasswordController extends Controller
         }
 
         return response()->json(['message' => 'The password reset token is valid. You can proceed to reset your password.'], 200);
+    }
+
+    /**
+     * Verify the password reset token.
+     *
+     * @param string $token
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function verifyPasswordResetToken($token)
+    {
+        if (empty($token)) {
+            return response()->json(['message' => 'Invalid token.'], 400);
+        }
+
+        $tokenRecord = PasswordResetToken::where('token', $token)->first();
+
+        if (!$tokenRecord) {
+            return response()->json(['message' => 'Invalid or expired token.'], 404);
+        }
+
+        if ($tokenRecord->used) {
+            return response()->json(['message' => 'This password reset token has already been used.'], 400);
+        }
+
+        if (Carbon::parse($tokenRecord->expires_at)->isPast()) {
+            return response()->json(['message' => 'This password reset token has expired.'], 400);
+        }
+
+        return response()->json(['status' => 200, 'message' => 'The token is valid.']);
     }
 
     // ... (rest of the code in the controller)
