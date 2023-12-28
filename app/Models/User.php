2dd4eec07553625eb1924864253fbf1a791f0aa9
Griselda (Jitera) <developers@jitera.com>
2023-12-28 05:50:31 +0000

    feat[database]: update table 'users' and create table 'requests' and create table 'request_images' Changes * '/database/migrations/2023_04_02_000000_update_requests_table_add_columns.php': created * '/app/Models/RequestImage.php': created * '/app/Models/Request.php': created * '/database/migrations/2023_04_03_000000_add_remember_token_to_users_table.php': created * '/app/Models/User.php': updated * '/database/migrations/2023_04_02_000000_add_image_path_request_id_to_request_images_table.php': created

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -65,5 +64,11 @@ class User extends Authenticatable
     public function passwordResetToken() // Relationship name changed to singular form
     {
         return $this->hasOne(PasswordResetToken::class, 'user_id');
+    }
+
+    // Define the one-to-many relationship with Request
+    public function requests()
+    {
+        return $this->hasMany(Request::class, 'user_id');
     }
 }
