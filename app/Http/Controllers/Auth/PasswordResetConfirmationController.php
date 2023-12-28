67e82fc7e94c9e1fa3d5b1f3b7fb392cff9beb93
Louise (Jitera) <developers@jitera.com>
2023-12-28 04:31:57 +0000

    feat[business-logic]: implement 'Password reset confirmation' Changes * '/app/Http/Controllers/Auth/PasswordResetConfirmationController.php': + In the `confirmReset` method, ensure that all required fields ('email', 'token', 'password', 'password_confirmation') are validated for presence and correctness. Use Laravel's built-in validation rules such as 'required', 'email', 'string', 'min', and 'c...

--- app/Http/Controllers/Auth/PasswordResetConfirmationController.php
+++ app/Http/Controllers/Auth/PasswordResetConfirmationController.php
@@ -8,6 +8,8 @@ use App\Models\PasswordResetToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class PasswordResetConfirmationController extends Controller
 {
@@ -24,6 +26,11 @@ class PasswordResetConfirmationController extends Controller
             return response()->json(['message' => $validator->errors()], 400);
         }
 
+        // Check if the new password and password confirmation match
+        if ($request->password !== $request->password_confirmation) {
+            return response()->json(['message' => 'Password confirmation does not match.'], 400);
+        }
+
         // Find the token and ensure it matches the provided email
         $passwordResetToken = PasswordResetToken::where('token', $request->token)
             ->where('email', $request->email)
@@ -37,15 +44,25 @@ class PasswordResetConfirmationController extends Controller
 
         // Find the user by email and reset the password
         $user = User::where('email', $request->email)->first();
-        $user->password = Hash::make($request->password);
-        if ($user->password_reset_required ?? false) { // Check if the field exists and is true
-            $user->password_reset_required = false; // Set to false if the field exists
+        if (!$user) {
+            return response()->json(['message' => 'User not found.'], 404);
         }
-        $user->save();
 
-        // Invalidate the used token by deleting it
-        $passwordResetToken->delete(); // Changed from marking as used to deleting
+        // Begin transaction to ensure atomicity
+        DB::beginTransaction();
+        try {
+            $user->password = Hash::make($request->password);
+            $user->password_reset_required = false; // Set to false as per requirement
+            $user->save();
 
-        return response()->json(['status' => 200, 'message' => 'Password has been reset successfully.'], 200);
+            // Invalidate the used token by deleting it
+            $passwordResetToken->delete(); // Changed from marking as used to deleting
+
+            DB::commit();
+            return response()->json(['status' => 200, 'message' => 'Password has been reset successfully.'], 200);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(['message' => 'An error occurred while resetting the password.'], 500);
+        }
     }
 }
