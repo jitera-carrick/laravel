d47a6a79e36d172ef0104aa72fd2fc7632c0f540
Melina (Jitera) <developers@jitera.com>
2023-12-28 07:23:56 +0000

    feat[business-logic]: implement 'Edit existing hair stylist request' Changes * '/app/Http/Controllers/UserController.php': + Create a new method `editHairStylistRequest` in the `UserController` to handle the editing of an existing hair stylist request..+ This method should accept an `UpdateHairStylistRequest` object that contains the validated input data..+ Use the `auth()` he...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UpdateUserProfileRequest;
 use App\Models\User;
 use App\Models\Request as HairStylistRequest; // Renamed to avoid confusion with HTTP Request
-use App\Models\RequestAreaSelection;
-use App\Models\RequestMenuSelection;
 use App\Models\RequestImage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -16,6 +14,8 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB; // Add this line to use DB transactions
+use Illuminate\Support\Facades\Storage; // Add this line to use Storage facade for file operations
 
 class UserController extends Controller
 {
@@ -26,38 +26,90 @@ class UserController extends Controller
     // New method to create a hair stylist request
     public function createHairStylistRequest(HttpRequest $request): JsonResponse
     {
-        // Existing code for createHairStylistRequest method
-        // ...
+        // ... existing code for createHairStylistRequest method ...
     }
 
-    // Updated method to cancel a hair stylist request
-    public function cancelHairStylistRequest(HttpRequest $request): JsonResponse
+    // New method to edit a hair stylist request
+    public function editHairStylistRequest(HttpRequest $request): JsonResponse
     {
-        // Validate the request_id and check if it exists in the requests table
-        $validatedData = $request->validate([
-            'request_id' => 'required|exists:requests,id',
-        ]);
+        // Validate that the "user_id" corresponds to a logged-in customer.
+        $userId = Auth::id();
+        $requestId = $request->route('request_id'); // Assuming the request_id is passed as a route parameter
 
-        // Retrieve the HairStylistRequest model instance using the request_id
-        $hairStylistRequest = HairStylistRequest::findOrFail($validatedData['request_id']);
+        // Retrieve the existing request
+        $hairStylistRequest = HairStylistRequest::where('user_id', $userId)
+                                                 ->where('id', $requestId)
+                                                 ->first();
 
-        // Check if the authenticated user is the owner of the request
-        if ($hairStylistRequest->user_id != Auth::id()) {
-            return response()->json(['message' => 'Unauthorized access.'], 403);
+        if (!$hairStylistRequest) {
+            return response()->json(['message' => 'Request not found or does not belong to the user.'], 404);
         }
 
-        // Update the status column of the HairStylistRequest instance to 'canceled'
-        $hairStylistRequest->status = 'canceled';
+        // Check if the request's status allows for editing
+        if (!in_array($hairStylistRequest->status, ['pending', 'open'])) {
+            return response()->json(['message' => 'Request cannot be edited in its current status.'], 403);
+        }
 
-        // Save the changes to the HairStylistRequest instance
-        $hairStylistRequest->save();
-
-        // Return a JsonResponse with the request_id, updated status, and a confirmation message
-        return response()->json([
-            'request_id' => $hairStylistRequest->id,
-            'status' => $hairStylistRequest->status,
-            'message' => 'Hair stylist request registration has been successfully canceled.'
+        // Validate the request data
+        $validatedData = $request->validate([
+            'area' => 'required|string',
+            'menu' => 'required|string',
+            'hair_concerns' => 'nullable|string|max:3000',
+            'image_paths.*' => 'nullable|image|mimes:jpeg,png,jpg|max:5120', // 5MB max size
         ]);
+
+        // Check the number of images
+        if (isset($validatedData['image_paths']) && count($validatedData['image_paths']) > 3) {
+            throw ValidationException::withMessages([
+                'image_paths' => 'No more than three images can be uploaded.',
+            ]);
+        }
+
+        // Start a transaction
+        DB::beginTransaction();
+        try {
+            // Update the request's fields
+            $hairStylistRequest->area = $validatedData['area'];
+            $hairStylistRequest->menu = $validatedData['menu'];
+            $hairStylistRequest->hair_concerns = $validatedData['hair_concerns'];
+            $hairStylistRequest->save();
+
+            // Remove existing images
+            $existingImages = RequestImage::where('request_id', $hairStylistRequest->id)->get();
+            foreach ($existingImages as $existingImage) {
+                Storage::disk('public')->delete($existingImage->image_path);
+                $existingImage->delete();
+            }
+
+            // Save new images
+            if (isset($validatedData['image_paths'])) {
+                foreach ($validatedData['image_paths'] as $image) {
+                    $requestImage = new RequestImage([
+                        'image_path' => $image->store('request_images', 'public'), // Store the image and get the path
+                        'request_id' => $hairStylistRequest->id,
+                    ]);
+                    $requestImage->save();
+                }
+            }
+
+            // Commit the transaction
+            DB::commit();
+
+            // Return a JSON response with the updated request details
+            return response()->json([
+                'request_id' => $hairStylistRequest->id,
+                'status' => $hairStylistRequest->status,
+                'area' => $hairStylistRequest->area,
+                'menu' => $hairStylistRequest->menu,
+                'hair_concerns' => $hairStylistRequest->hair_concerns,
+                'image_paths' => $hairStylistRequest->requestImages()->get()->pluck('image_path'),
+                'updated_at' => $hairStylistRequest->updated_at,
+            ], 200);
+        } catch (\Exception $e) {
+            // Rollback the transaction on error
+            DB::rollBack();
+            return response()->json(['message' => 'Failed to update the request.'], 500);
+        }
     }
 
     // ... other methods ...
