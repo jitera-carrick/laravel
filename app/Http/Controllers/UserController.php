cb7766cba3e79bf179410c9645b43ea4b08febe9
Carola (Jitera) <developers@jitera.com>
2023-12-28 07:10:15 +0000

    feat[business-logic]: implement 'Edit existing hair stylist request' Changes * '/app/Http/Controllers/UserController.php': + Create a method `updateHairStylistRequest` in `UserController` to handle the editing of an existing hair stylist request..+ This method should accept `request_id`, `user_id`, `area`, `menu`, `hair_concerns`, and `image_path` as parameters..+ Begin by val...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,8 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -40,33 +42,34 @@ class UserController extends Controller
             'image_paths.*' => 'nullable|image|mimes:jpeg,png,jpg|max:5120', // 5MB max size
         ]);
 
-        // Check the number of images
-        if (isset($validatedData['image_paths']) && count($validatedData['image_paths']) > 3) {
-            throw ValidationException::withMessages([
-                'image_paths' => 'No more than three images can be uploaded.',
-            ]);
-        }
-
         // Create a new HairStylistRequest instance and fill it with validated data
         $hairStylistRequest = new HairStylistRequest([
             'user_id' => $validatedData['user_id'],
             'area' => $validatedData['area'],
             'menu' => $validatedData['menu'],
             'hair_concerns' => $validatedData['hair_concerns'],
-            'status' => 'open', // Default status set to 'open'
+            'status' => 'pending', // Default status
         ]);
 
         // Save the new request
         $hairStylistRequest->save();
 
+        // Associate area selections with the request
+        $hairStylistRequest->requestAreaSelections()->create([
+            'area_id' => $validatedData['area'],
+        ]);
+
+        // Associate menu selections with the request
+        $hairStylistRequest->requestMenuSelections()->create([
+            'menu_id' => $validatedData['menu'],
+        ]);
+
         // Save images associated with the request
         if (isset($validatedData['image_paths'])) {
-            foreach ($validatedData['image_paths'] as $image) {
-                $requestImage = new RequestImage([
-                    'image_path' => $image->store('request_images', 'public'), // Store the image and get the path
-                    'request_id' => $hairStylistRequest->id,
+            foreach ($validatedData['image_paths'] as $imagePath) {
+                $hairStylistRequest->requestImages()->create([
+                    'image_path' => $imagePath,
                 ]);
-                $requestImage->save();
             }
         }
 
@@ -74,12 +77,73 @@ class UserController extends Controller
         return response()->json([
             'request_id' => $hairStylistRequest->id,
             'status' => $hairStylistRequest->status,
-            'area' => $hairStylistRequest->area,
-            'menu' => $hairStylistRequest->menu,
+            'area_selections' => $hairStylistRequest->requestAreaSelections,
+            'menu_selections' => $hairStylistRequest->requestMenuSelections,
             'hair_concerns' => $hairStylistRequest->hair_concerns,
-            'image_paths' => $hairStylistRequest->requestImages()->get()->pluck('image_path'), // Get only the image paths
+            'image_paths' => $hairStylistRequest->requestImages,
             'created_at' => $hairStylistRequest->created_at,
         ], 201);
+    }
+
+    // New method to update a hair stylist request
+    public function updateHairStylistRequest(HttpRequest $request): JsonResponse
+    {
+        // Validate that the "user_id" corresponds to the currently authenticated user
+        // and that the "request_id" exists and belongs to that user.
+        $user = Auth::user();
+        $hairStylistRequest = HairStylistRequest::where('id', $request->request_id)
+                                                ->where('user_id', $user->id)
+                                                ->first();
+
+        if (!$hairStylistRequest) {
+            return response()->json(['message' => 'Request not found or access denied.'], 404);
+        }
+
+        // Validate the input data
+        $validator = Validator::make($request->all(), [
+            'area' => 'sometimes|required|string',
+            'menu' => 'sometimes|required|string',
+            'hair_concerns' => 'sometimes|nullable|string|max:3000',
+            'image_path.*' => 'sometimes|nullable|image|mimes:jpeg,png,jpg|max:5120', // 5MB max size
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        // Update the request with the validated data
+        $hairStylistRequest->fill($validatedData);
+        $hairStylistRequest->save();
+
+        // Handle image_path updates
+        if (isset($validatedData['image_path'])) {
+            // Delete old images
+            foreach ($hairStylistRequest->requestImages as $existingImage) {
+                Storage::delete($existingImage->image_path);
+                $existingImage->delete();
+            }
+
+            // Add new images
+            foreach ($validatedData['image_path'] as $image) {
+                $path = $image->store('request_images', 'public');
+                $hairStylistRequest->requestImages()->create([
+                    'image_path' => $path,
+                ]);
+            }
+        }
+
+        // Return a success message with the details of the updated request
+        return response()->json([
+            'message' => 'Request updated successfully.',
+            'request' => [
+                'area' => $hairStylistRequest->area,
+                'menu' => $hairStylistRequest->menu,
+                'hair_concerns' => $hairStylistRequest->hair_concerns,
+                'image_paths' => $hairStylistRequest->requestImages,
+            ],
+        ], 200);
     }
 
     // ... other methods ...
