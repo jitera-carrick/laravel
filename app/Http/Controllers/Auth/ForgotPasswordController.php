3288099f3a3fd43e46f3c709443ee16f7249cda1
Lauri (Jitera) <developers@jitera.com>
2023-12-28 03:26:11 +0000

    feat[api]: implement 'Initiate Password Reset' Changes * '/routes/api.php': + In the `api.php` file, add a new route to handle the POST request for initiating the password reset..+ Use the `Route::post` method to define the route. The first parameter is the endpoint `/api/users/password-reset/initiate`, and the second parameter is... * '/app/Http/Controllers/Auth/ForgotPasswordController.php': + In the existing ForgotPasswordController, add a new method named `initiatePasswordReset` to handle the password reset initiation logic..+ Import the necessary classes at the top of the file if they are not already imported. You will need `Illuminate\Http...

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -9,10 +9,57 @@ use App\Models\PasswordResetToken;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Notification;
 use App\Notifications\ResetPasswordNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
     // Add your new method below
+    public function initiatePasswordReset(Request $request)
+    {
+        // Validate the email format
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Invalid email address.'], 400);
+        }
+
+        // Check if the email exists in the users table
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'No user found with that email.'], 404);
+        }
+
+        // Generate a unique password reset token
+        $token = Str::random(60);
+
+        // Store the token in the password_reset_tokens table
+        $passwordResetToken = new PasswordResetToken([
+            'email' => $user->email,
+            'token' => $token,
+            'created_at' => now(),
+            'expires_at' => now()->addHours(2),
+            'used' => false,
+            'user_id' => $user->id
+        ]);
+        $passwordResetToken->save();
+
+        // Send the password reset token to the user's email
+        try {
+            $user->notify(new ResetPasswordNotification($token));
+        } catch (\Exception $e) {
+            // Handle the exception
+            return response()->json(['message' => 'Failed to send password reset email.'], 500);
+        }
+
+        // Return a response indicating the password reset email has been sent successfully
+        return response()->json(['message' => 'Password reset instructions have been sent to your email.'], 200);
+    }
+
+    // The sendResetLinkEmail method remains unchanged
     public function sendResetLinkEmail(EmailRequest $request)
     {
         // Validate the email format
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Password;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Added new route for initiating password reset
+Route::post('/users/password-reset/initiate', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => 400,
+            'message' => 'Invalid email address.'
+        ], 400);
+    }
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user) {
+        return response()->json([
+            'status' => 404,
+            'message' => 'The email address is not associated with any user account.'
+        ], 404);
+    }
+
+    $status = Password::sendResetLink($request->only('email'));
+
+    if ($status === Password::RESET_LINK_SENT) {
+        return response()->json([
+            'status' => 200,
+            'message' => 'Password reset instructions have been sent to your email.'
+        ], 200);
+    }
+
+    return response()->json([
+        'status' => 500,
+        'message' => 'An unexpected error occurred on the server.'
+    ], 500);
 });
