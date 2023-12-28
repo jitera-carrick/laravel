735d39145b0b31fd7f95243d1b404c2dc1dae159
Tobey (Jitera) <developers@jitera.com>
2023-12-28 04:47:43 +0000

    feat[api]: implement 'Validate Password Reset Token' Changes * '/routes/api.php': + In the `api.php` file, add a new route that will handle the GET request for validating the password reset token..+ Use the `Route` facade to define the route. The route should point to the `validateResetToken` method in the `ResetPasswordController`..+ S... * '/app/Http/Controllers/Auth/ResetPasswordController.php': + In the existing ResetPasswordController, add a new public method named `validateResetToken` which will handle the GET request to validate the password reset token..+ Import necessary classes at the top of the file if they are not already imported. For ex...

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ResetPasswordRequest;
 use App\Services\PasswordResetService;
 use App\Utils\ApiResponse;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Carbon;
@@ -26,50 +27,73 @@ class ResetPasswordController extends Controller
     {
         $input = $request->only(['email', 'token', 'password', 'password_confirmation']);
 
-        // Check if password confirmation matches
         if ($input['password'] !== $input['password_confirmation']) {
             return ApiResponse::error(['message' => 'Password confirmation does not match.'], 422);
         }
 
-        // Begin transaction
         DB::beginTransaction();
         try {
-            // Find the token in the password_reset_tokens table
             $passwordResetToken = PasswordResetToken::where('email', $input['email'])
                 ->where('token', $input['token'])
                 ->where('used', false)
-                ->where('created_at', '>', Carbon::now()->subHours(2)) // Assuming tokens are valid for 2 hours
+                ->where('created_at', '>', Carbon::now()->subHours(2))
                 ->first();
 
             if (!$passwordResetToken) {
                 return ApiResponse::error(['message' => 'Invalid or expired password reset token.'], 422);
             }
 
-            // Hash the new password
             $newPasswordHash = Hash::make($input['password']);
 
-            // Update the user's password
             $user = User::where('email', $input['email'])->first();
             $user->password = $newPasswordHash;
             $user->last_password_reset = Carbon::now();
             $user->save();
 
-            // Mark the token as used
             $passwordResetToken->used = true;
             $passwordResetToken->save();
 
-            // Commit transaction
             DB::commit();
 
-            // Send confirmation email (pseudo code, assuming MailService exists)
             // MailService::sendPasswordResetConfirmation($user->email);
 
             return ApiResponse::success(['message' => 'Password has been successfully reset.']);
         } catch (\Exception $e) {
-            // Rollback transaction
             DB::rollBack();
             return ApiResponse::error(['message' => 'An error occurred while resetting the password.'], 500);
         }
+    }
+
+    /**
+     * Validate the password reset token.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function validateResetToken(Request $request): JsonResponse
+    {
+        $email = $request->input('email');
+        $token = $request->input('token');
+
+        if (!$email) {
+            return ApiResponse::error(['message' => 'Invalid email address.'], 400);
+        }
+
+        if (!$token) {
+            return ApiResponse::error(['message' => 'Invalid or expired password reset token.'], 422);
+        }
+
+        $passwordResetToken = PasswordResetToken::where('email', $email)
+            ->where('token', $token)
+            ->where('used', false)
+            ->where('created_at', '>', Carbon::now()->subHours(2))
+            ->first();
+
+        if (!$passwordResetToken) {
+            return ApiResponse::error(['message' => 'Invalid or expired password reset token.'], 404);
+        }
+
+        return ApiResponse::success(['message' => 'The password reset token is valid.']);
     }
 
     // ... other methods ...
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// New route for validating the password reset token
+Route::get('/users/validate-reset-token', [ResetPasswordController::class, 'validateResetToken']);
