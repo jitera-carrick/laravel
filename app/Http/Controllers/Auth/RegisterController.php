7e1dccb01ba5dcdc2a8ca322503bab49469fdc6d
Xylia (Jitera) <developers@jitera.com>
2023-12-28 03:32:04 +0000

    feat[api]: implement 'User Registration' Changes * '/routes/api.php': + Add a new POST route to the api.php file that points to the `register` method in the RegisterController. The endpoint should be '/api/users/register'..+ Since this is a registration route, it should not be protected by the 'auth:sanctum' middleware. Ther... * '/app/Http/Controllers/Auth/RegisterController.php': + In the existing RegisterController, add a new method called `register` if it doesn't already exist. Since the example method already exists, you can modify it to meet the new requirements..+ Update the validation rules within the `register` method to inc...

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,34 +19,63 @@ class RegisterController extends Controller
 
     public function register(RegisterRequest $request)
     {
-        // Validate the request
+        // Validate the request with custom error messages
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+        ], [
+            'name.required' => 'The name is required.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'Invalid email format.',
+            'email.unique' => 'Email already registered.',
+            'password.required' => 'The password field is required.',
+            'password.min' => 'Password must be at least 8 characters.',
+            'password.confirmed' => 'The password confirmation does not match.',
         ]);
 
         if ($validator->fails()) {
-            return Response::json(['errors' => $validator->errors()], 422);
+            return Response::json([
+                'status' => 'error',
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
         $validatedData = $validator->validated();
 
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-            'remember_token' => Str::random(60),
-            // 'created_at' and 'updated_at' will be set automatically by Eloquent
-        ]);
+        try {
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+                'remember_token' => Str::random(60),
+                // 'created_at' and 'updated_at' will be set automatically by Eloquent
+            ]);
 
-        // Dispatch an email verification job
-        Queue::push(function ($job) use ($user) {
-            Mail::to($user->email)->send(new EmailVerification($user));
-            $job->delete();
-        });
+            // Dispatch an email verification job
+            Queue::push(function ($job) use ($user) {
+                Mail::to($user->email)->send(new EmailVerification($user));
+                $job->delete();
+            });
 
-        return Response::json(['user_id' => $user->id], 201);
+            return Response::json([
+                'status' => 201,
+                'message' => 'User registered successfully.',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'created_at' => $user->created_at->toIso8601String(),
+                ]
+            ], 201);
+        } catch (\Exception $e) {
+            return Response::json([
+                'status' => 'error',
+                'message' => 'Server error',
+                'errors' => $e->getMessage()
+            ], 500);
+        }
     }
 
     // ... (other methods)
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController; // Import UserController
-use App\Http\Controllers\ShopController; // Import ShopController
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +15,52 @@ use App\Http\Controllers\ShopController; // Import ShopController
 |
 */
 
+// Existing route for getting user details
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // New route for updating shop details
-Route::middleware('auth:sanctum')->match(['put', 'patch'], '/shop/update', [ShopController::class, 'updateShop']);
+Route::middleware('auth:sanctum')->match(['put', 'patch'], '/shop/update', 'ShopController@updateShop');
 
-// New route for updating user profile
-Route::middleware('auth:sanctum')->put('/users/profile', [UserController::class, 'updateProfile']);
+// New route for user registration
+Route::post('/users/register', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|min:8',
+    ], [
+        'name.required' => 'The name is required.',
+        'email.required' => 'The email is required.',
+        'email.email' => 'Invalid email format.',
+        'email.unique' => 'Email already registered.',
+        'password.required' => 'The password is required.',
+        'password.min' => 'Password must be at least 8 characters.',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => 400,
+            'message' => 'Bad Request',
+            'errors' => $validator->errors(),
+        ], 400);
+    }
+
+    // Assuming User model exists and has a create method to handle user creation.
+    $user = \App\Models\User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password), // Password should be hashed
+    ]);
+
+    return response()->json([
+        'status' => 201,
+        'message' => 'User registered successfully.',
+        'user' => [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'created_at' => $user->created_at->toIso8601String(),
+        ],
+    ], 201);
+});
