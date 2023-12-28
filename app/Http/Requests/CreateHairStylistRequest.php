e9457ef6040dbf8c49a8c3fe64d71e27efc4ee93
Gisella (Jitera) <developers@jitera.com>
2023-12-28 07:32:29 +0000

    feat[business-logic]: implement 'Create hair stylist request' Changes * '/app/Http/Requests/CreateHairStylistRequest.php': + Define a new form request validation class `CreateHairStylistRequest` to handle the validation of the input data for creating a hair stylist request..+ In the `rules` method, specify the validation rules:.  - "user_id" should be an integer and exist in t... * '/app/Http/Controllers/UserController.php': + Create a new method `createHairStylistRequest` in the `UserController` to handle the creation of hair stylist requests..+ Use the `auth` middleware to ensure that the user is authenticated before proceeding with the request creation..+ Validate the input...

---
+++ app/Http/Requests/CreateHairStylistRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class CreateHairStylistRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Check if the user is authenticated
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|integer|exists:users,id',
+            'area' => 'required|string',
+            'menu' => 'required|string',
+            'hair_concerns' => 'string|max:3000',
+            'image_paths' => 'array|max:3',
+            'image_paths.*' => 'file|mimes:png,jpg,jpeg|max:5120', // 5MB
+        ];
+    }
+
+    /**
+     * Get the custom messages for validation errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'user_id.required' => 'The user id field is required.',
+            'user_id.integer' => 'The user id must be an integer.',
+            'user_id.exists' => 'The selected user id is invalid.',
+            'area.required' => 'The area field is required.',
+            'menu.required' => 'The menu field is required.',
+            'hair_concerns.max' => 'The hair concerns may not be greater than 3000 characters.',
+            'image_paths.array' => 'The image paths must be an array.',
+            'image_paths.max' => 'The image paths may not have more than 3 items.',
+            'image_paths.*.file' => 'Each image path must be a file.',
+            'image_paths.*.mimes' => 'Each file must be of type: png, jpg, jpeg.',
+            'image_paths.*.max' => 'Each file may not be greater than 5MB.',
+        ];
+    }
+}
