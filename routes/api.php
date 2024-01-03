18ff163e1b0684e065ba233c50d5a7830f33bee8
nhatnkv <hongnhat.nguyen@jitera.com>
2024-01-03 12:17:34 +0700

    stash

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // New route for resetting the user's password
-Route::post('/users/reset-password', [ResetPasswordController::class, 'resetPassword']);
+Route::post("/users/reset-password", [ResetPasswordController::class, "resetPassword"]);
 
 // New route for user registration with throttle middleware
-Route::post("/users/register", [RegisterController::class, "register"])->middleware("throttle:api");
+Route::post('/users/register', [RegisterController::class, "register"])->middleware("throttle:api");
 
 // New route to handle the DELETE request for the endpoint `/api/requests/{request_id}/images/{image_id}`
 Route::middleware('auth:sanctum')->delete('/requests/{request_id}/images/{image_id}', [UserController::class, 'deleteRequestImage']);
