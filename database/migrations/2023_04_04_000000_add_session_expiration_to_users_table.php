6d7c767352409959f548713dfa9a074633fea6ee
Roberta (Jitera) <developers@jitera.com>
2024-01-04 03:14:07 +0000

    feat[database]: delete tables 'password_reset_requests', 'stylist_requests' and update table 'users' Changes * '/database/migrations/2023_04_04_000000_add_session_expiration_to_users_table.php': created * '/app/Models/User.php': updated + '/app/Http/Requests/CreateHairStylistRequest.php': deleted + '/app/Http/Requests/UpdateHairStylistRequest.php': deleted + '/app/Models/PasswordResetRequest.php': deleted + '/app/Models/StylistRequest.php': deleted + '/database/migrations/2023_04_02_000000_add_new_columns_to_password_reset_requests_table.php': deleted + '/database/migrations/2023_04_02_000000_add_new_columns_to_stylist_requests_table.php': deleted

--- database/migrations/2023_04_04_000000_add_session_expiration_to_users_table.php
+++ database/migrations/2023_04_04_000000_add_session_expiration_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            // Add new column for session_expiration
+            // Add new column for session expiration
             $table->timestamp('session_expiration')->nullable()->after('session_token');
         });
     }
