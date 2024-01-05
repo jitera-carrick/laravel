0e9e83c484361c01f363f98ba5b2d32c45bdbb1b
Lorilee (Jitera) <developers@jitera.com>
2024-01-05 05:46:19 +0000

    feat[database]: delete tables 'email_verification_tokens' and update table 'users' Changes * '/app/Models/User.php': updated * '/database/migrations/2023_04_04_000000_create_users_table.php': created + '/app/Http/Controllers/Auth/EmailVerificationController.php': deleted + '/app/Http/Controllers/Auth/ForgotPasswordController.php': deleted + '/app/Http/Controllers/Auth/ResetPasswordController.php': deleted + '/app/Http/Controllers/Auth/VerifyEmailController.php': deleted + '/app/Models/PasswordResetRequest.php': deleted + '/app/Models/PasswordResetToken.php': deleted + '/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php': deleted

---
+++ database/migrations/2023_04_04_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('password_hash');
+            $table->string('password_salt');
+            $table->timestamp('last_password_reset')->nullable();
+            $table->rememberToken();
+            $table->string('session_token')->nullable();
+            $table->boolean('is_logged_in')->default(false);
+            $table->timestamp('session_expiration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
