c9b2aa28c854c17d6460ace7c94df9095744a2f0
Christean (Jitera) <developers@jitera.com>
2024-01-09 05:34:14 +0000

    feat[api]: implement Cancel Login Process

--- routes/api.php
+++ routes/api.php
@@ -43,25 +43,23 @@ Route::middleware('auth:sanctum')->delete('/user/hair-stylist-request/image', [R
 Route::middleware('auth:sanctum')->delete('/stylist-requests/{id}', [StylistRequestController::class, 'cancelStylistRequest'])
     ->where('id', '[0-9]+')
     ->name('stylist-requests.cancel');
+
 Route::middleware('auth:sanctum')->post('/cancel-login', [AuthController::class, 'cancelLogin'])->name('auth.cancel-login');
+
 Route::middleware('auth:sanctum')->post('/hair-stylist-requests/create', [HairStylistRequestController::class, 'createHairStylistRequest']);
 
 Route::middleware('auth:sanctum')->delete('/requests/images/{request_image_id}', [RequestImageController::class, 'deleteRequestImage']);
 
 Route::middleware('auth:sanctum')->match(['put', 'patch'], '/hair-stylist-requests/{id}', [HairStylistRequestController::class, 'updateHairStylistRequest']);
 
-// The following route is from the existing code but was updated in the new code to use POST instead of PUT.
-// Since the new code specifies a POST route for creating stylist requests, we will keep the new code's version.
-// This is a decision based on the assumption that the new code's change was intentional and should override the existing code.
 Route::middleware('auth:sanctum')->post('/stylist-request/create', [StylistRequestController::class, 'createStylistRequest']);
 
-// The existing PUT route for updating stylist requests is kept to maintain backward compatibility.
-// Clients using the old endpoint will still be able to update stylist requests.
 Route::middleware('auth:sanctum')->put('/stylist-request/update/{id}', [StylistRequestController::class, 'update'])
     ->where('id', '[0-9]+')
     ->name('stylist-request.update');
 
-// The POST route for canceling stylist requests from the new code is kept.
-// This change is assumed to be intentional and should override the existing code.
 Route::middleware('auth:sanctum')->post('/stylist-request/cancel/{id}', [StylistRequestController::class, 'cancelStylistRequest'])
     ->where('id', '[0-9]+');
+
+// The new route for canceling the login process as per the requirement
+Route::post('/api/login/cancel', [SessionController::class, 'cancelLoginProcess']);
